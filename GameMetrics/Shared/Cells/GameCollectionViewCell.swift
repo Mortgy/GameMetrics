aef10c679184651aa80c6b35fa3c008442371092
Muhammed Mortgy <m@mortgy.com>
2021-03-18 21:02:58 +0300

    create GameCollectionViewCell

--- GameMetrics/Shared/Cells/GameCollectionViewCell.swift
+++ GameMetrics/Shared/Cells/GameCollectionViewCell.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class GameCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var gameTitleLabel: UILabel!
+    @IBOutlet weak var gameScoreLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
