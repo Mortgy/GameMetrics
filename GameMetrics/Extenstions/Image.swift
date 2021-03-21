a521a87da3b675cfd0c213fd30ed7b9fa5a2dd2e
Muhammed Mortgy <m@mortgy.com>
2021-03-22 02:27:15 +0300

    change loading thread to background for smoother scrolling ( later setting image via main thread )

--- GameMetrics/Extenstions/Image.swift
+++ GameMetrics/Extenstions/Image.swift
@@ -11,21 +11,20 @@ extension UIImageView {
     func load(url: URL) {
         let urlMd5 = url.absoluteString.MD5()
         
-        if let data: Data = DiskCacheManager().value(urlMd5, from: .images) {
-            self.image = UIImage(data: data)
-        } else {
-            DispatchQueue.global().async { [weak self] in
-                if let data = try? Data(contentsOf: url) {
-                    if let image = UIImage(data: data) {
-                        DispatchQueue.main.async {
-                            self?.image = image
-                            DiskCacheManager().add(data, for: url.absoluteString.MD5(), to: .images)
-                        }
+        DispatchQueue.global().async { [weak self] in
+            if let data: Data = DiskCacheManager().value(urlMd5, from: .images) {
+                DispatchQueue.main.async {
+                    self?.image = UIImage(data: data)
+                }
+            } else if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                        DiskCacheManager().add(data, for: url.absoluteString.MD5(), to: .images)
                     }
                 }
             }
         }
-        
     }
 }
 
---
+++ GameMetrics/Screens/Details/ViewModel/GameDetailsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  GameDetailsViewModel.swift
+//  GameMetrics
+//
+//  Created by Mortgy on 3/21/21.
+//
+
+import UIKit
+//
+//class GameDetailsViewModel: ViewModel {
+//
+//}
