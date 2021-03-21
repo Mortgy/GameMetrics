1e7527033e66995e52f5e7cda028d9f6c05e179d
Muhammed Mortgy <m@mortgy.com>
2021-03-21 20:45:19 +0300

    create image and notifications extensions

---
+++ GameMetrics/Extenstions/Image.swift
@@ -0,0 +1,22 @@
+//
+//  Image.swift
+//  GameMetrics
+//
+//  Created by Mortgy on 3/21/21.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GameMetrics/Extenstions/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  GameMetrics
+//
+//  Created by Mortgy on 3/21/21.
+//
+
+import UIKit
+
+extension Notification.Name {
+    static let seenItemUpdated = Notification.Name("seenItemUpdated")
+    static let favoritedItemUpdated = Notification.Name("favoritedItemUpdated")
+}
