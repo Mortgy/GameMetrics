8d9d60f3aa71907792f10806d5566369bf25ec55
Muhammed Mortgy <m@mortgy.com>
2021-03-23 00:01:53 +0300

    Unit testing started

---
+++ GameMetricsTests/CacheManager/DiskCacheManagerTests.swift
@@ -0,0 +1,13 @@
+//
+//  DiskCacheManagerTests.swift
+//  GameMetricsTests
+//
+//  Created by Mortgy on 3/22/21.
+//
+
+import XCTest
+@testable import GameMetrics
+
+class DiskCacheManagerTests: XCTestCase {
+    
+}
---
+++ GameMetricsTests/CacheManager/MemoryCacheManagerTests.swift
@@ -0,0 +1,58 @@
+//
+//  MemoryCacheManagerTests.swift
+//  GameMetricsTests
+//
+//  Created by Mortgy on 3/22/21.
+//
+
+import XCTest
+@testable import GameMetrics
+
+class MemoryCacheManagerTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        MemoryCacheManager().pop(value: "test", forList: .seen)
+
+    }
+    
+    func testPop() throws {
+        
+        MemoryCacheManager().pop(value: "test", forList: .seen)
+        let testCache: [String] = MemoryCacheManager().values(in: .seen)
+        XCTAssertEqual(testCache.count, 0, "Array count should be 0")
+        
+    }
+    
+    func testAppend() throws {
+        
+        MemoryCacheManager().append(value: "test", forList: .seen)
+        let testCache: [String] = MemoryCacheManager().values(in: .seen)
+        
+        XCTAssertEqual(testCache, ["test"])
+        XCTAssertEqual(testCache.count, 1, "Array has 1 element")
+        
+    }
+    
+    func testValueExists() throws {
+
+        MemoryCacheManager().append(value: "test", forList: .seen)
+        let valueExists = MemoryCacheManager().valueExists(value: "test", in: .seen)
+        XCTAssertTrue(valueExists, "test value exists")
+
+    }
+    
+    func testAppend2() throws {
+        
+        MemoryCacheManager().append(value: "test", forList: .seen)
+        MemoryCacheManager().append(value: "test", forList: .seen)
+        
+        let testCache: [String] = MemoryCacheManager().values(in: .seen)
+        XCTAssertEqual(testCache.count, 2, "Array count is 2")
+        XCTAssertGreaterThan(testCache.count, 1, "Array count greater than 1")
+        XCTAssertLessThan(testCache.count, 3, "Array count less than 3")
+
+    }
+    
+
+}
